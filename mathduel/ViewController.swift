178f3671e52a6d766f139ca8189ea9c193986f1c
Clementgann <clement@oceantech.asia>
2024-12-30 15:57:05 +0800

    Initial Commit

---
+++ mathduel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mathduel
+//
+//  Created by Clement Gan on 30/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
